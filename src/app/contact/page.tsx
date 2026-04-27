d1c447ac735b2eb96d5b58a9416f88805c2e2b40
Hiruni Praveesha <126430052+HiruniPraveesha@users.noreply.github.com>
2026-04-27 19:46:31 +0530

    Contact page issue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -164,7 +164,7 @@ export default function Contact() {
                         Heritage Care Residence
                         <br />
                         Diyagala Boys Town <br />
-                        Ragama, Sri Lanka
+                        Ragama, Sri Lanka 11350
                       </>
                     ),
                     icon: (
